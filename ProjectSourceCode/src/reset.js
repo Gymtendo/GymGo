7645d79fd18ac8409323b93ea1e018180f9a1495
Kevin Wang <kevnum01@gmail.com>
2025-04-23 22:47:02 -0600

    reset create.sql, created temporary reset.js to reset render db

---
+++ ProjectSourceCode/src/reset.js
@@ -0,0 +1,18 @@
+// reset.js
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+async function resetDB() {
+  await client.connect();
+  await client.query('DROP SCHEMA public CASCADE;');
+  await client.query('CREATE SCHEMA public;');
+  await client.query(require('fs').readFileSync('./src/init_data/create.sql', 'utf8'));
+  await client.query(require('fs').readFileSync('./src/init_data/insert.sql', 'utf8'));
+  await client.end();
+  console.log('Database reset complete.');
+}
+
+resetDB().catch(console.error);
